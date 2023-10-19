c940f139b6ce049429f43fc989419123c21823a7
Enilson Silva <enilson.silva@wicomm.com.br>
2023-10-19 11:44:01 -0300

    feat: product loop

---
+++ components/product/ProductCardCategory.tsx
@@ -0,0 +1,314 @@
+import { SendEventOnClick } from "$store/components/Analytics.tsx";
+import Avatar from "$store/components/ui/Avatar.tsx";
+import WishlistButton from "$store/islands/WishlistButton.tsx";
+import { formatPrice } from "$store/sdk/format.ts";
+import { useOffer } from "$store/sdk/useOffer.ts";
+import { usePlatform } from "$store/sdk/usePlatform.tsx";
+import { useVariantPossibilities } from "$store/sdk/useVariantPossiblities.ts";
+import AddToCartButtonVTEX from "$store/islands/AddToCartButton/Shelf/vtexShelf.tsx";
+import type { Product } from "apps/commerce/types.ts";
+import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalyticsItem.ts";
+import Image from "apps/website/components/Image.tsx";
+
+export interface Layout {
+  basics?: {
+    contentAlignment?: "Left" | "Center";
+    oldPriceSize?: "Small" | "Normal";
+    ctaText?: string;
+  };
+  elementsPositions?: {
+    skuSelector?: "Top" | "Bottom";
+    favoriteIcon?: "Top right" | "Top left";
+  };
+  hide?: {
+    productName?: boolean;
+    productDescription?: boolean;
+    allPrices?: boolean;
+    installments?: boolean;
+    skuSelector?: boolean;
+    cta?: boolean;
+  };
+  onMouseOver?: {
+    image?: "Change image" | "Zoom image";
+    card?: "None" | "Move up";
+    showFavoriteIcon?: boolean;
+    showSkuSelector?: boolean;
+    showCardShadow?: boolean;
+    showCta?: boolean;
+  };
+}
+
+interface Props {
+  product: Product;
+  /** Preload card image */
+  preload?: boolean;
+
+  /** @description used for analytics event */
+  itemListName?: string;
+  layout?: Layout;
+
+  platform: ReturnType<typeof usePlatform>;
+  index?: number;
+}
+
+const relative = (url: string) => {
+  const link = new URL(url);
+  return `${link.pathname}${link.search}`;
+};
+
+const WIDTH = 282;
+const HEIGHT = 353;
+
+function ProductCardCategory(
+  { product, preload, itemListName, layout, platform, index }: Props,
+) {
+  const {
+    url,
+    productID,
+    name = "",
+    image: images,
+    offers,
+    isVariantOf,
+  } = product;
+
+  const id = `product-card-${productID}`;
+  const productGroupID = isVariantOf?.productGroupID ?? "";
+
+  const imageNoMeasurements = images?.filter((image) =>
+    image.name !== "medida"
+  );
+
+  const front = index?.toString() && images ? images[index] : undefined;
+  const [back] = images ?? [];
+
+  const { listPrice, price = 0, installments, seller = "1" } = useOffer(offers);
+  const possibilities = useVariantPossibilities(product);
+  const discount = price && listPrice ? listPrice - price : 0;
+  const variants = Object.entries(Object.values(possibilities)[0] ?? {});
+
+  const l = layout;
+  const align =
+    !l?.basics?.contentAlignment || l?.basics?.contentAlignment == "Left"
+      ? "left"
+      : "center";
+  const skuSelector = variants.map(([value, [link]]) => (
+    <li>
+      <a href={link}>
+        <Avatar
+          variant={link === url ? "active" : "default"}
+          content={value}
+        />
+      </a>
+    </li>
+  ));
+  const cta = (
+    <a
+      href={url && relative(url)}
+      aria-label="view product"
+      class="btn btn-block"
+    >
+      {l?.basics?.ctaText || "Ver produto"}
+    </a>
+  );
+
+  return (
+    <div
+      id={id}
+      class={`card card-compact rounded-none  group gap-4 w-full ${
+        align === "center" ? "text-center" : "text-start"
+      } ${l?.onMouseOver?.showCardShadow ? "lg:hover:card-bordered" : ""}
+        ${
+        l?.onMouseOver?.card === "Move up" &&
+        "duration-500 transition-translate ease-in-out lg:hover:-translate-y-2"
+      }
+      `}
+      data-deco="view-product"
+    >
+      <SendEventOnClick
+        id={id}
+        event={{
+          name: "select_item" as const,
+          params: {
+            item_list_name: itemListName,
+            items: [
+              mapProductToAnalyticsItem({
+                product,
+                price,
+                listPrice,
+              }),
+            ],
+          },
+        }}
+      />
+      <figure
+        class="relative overflow-hidden"
+        style={{ aspectRatio: `${WIDTH} / ${HEIGHT}` }}
+      >
+        {/* Wishlist button */}
+        <div
+          class={`absolute transition ease-in-out duration-300  justify-between flex lg:hidden items-center bottom-2 z-10 lg:group-hover:flex right-2 left-2`}
+        >
+          {!l?.hide?.cta
+            ? (
+              <div
+                class={` flex items-end ${
+                  l?.onMouseOver?.showCta ? "lg:hidden" : ""
+                }`}
+              >
+                <AddToCartButtonVTEX
+                  name={name}
+                  productID={productID}
+                  productGroupID={productGroupID}
+                  price={price}
+                  discount={discount}
+                  seller={seller}
+                />
+              </div>
+            )
+            : ""}
+
+          {platform === "vtex" && (
+            <WishlistButton
+              productGroupID={productGroupID}
+              productID={productID}
+            />
+          )}
+        </div>
+        {/* Product Images */}
+        <a
+          href={url && relative(url)}
+          aria-label="view product"
+          class="grid grid-cols-1 grid-rows-1 w-full"
+        >
+          <Image
+            src={front?.url!}
+            alt={front?.alternateName}
+            width={WIDTH}
+            height={HEIGHT}
+            class={`bg-base-100 col-span-full row-span-full  w-full ${
+              l?.onMouseOver?.image == "Zoom image"
+                ? "duration-100 transition-scale scale-100 lg:group-hover:scale-125"
+                : ""
+            }`}
+            sizes="(max-width: 640px) 50vw, 20vw"
+            preload={preload}
+            loading={preload ? "eager" : "lazy"}
+            decoding="async"
+          />
+          {(!l?.onMouseOver?.image ||
+            l?.onMouseOver?.image == "Change image") && (
+            <Image
+              src={back?.url ?? front?.url!}
+              alt={back?.alternateName ?? front?.alternateName}
+              width={WIDTH}
+              height={HEIGHT}
+              class="bg-base-100 col-span-full row-span-full transition-opacity  w-full opacity-0 lg:group-hover:opacity-100"
+              sizes="(max-width: 640px) 50vw, 20vw"
+              loading="lazy"
+              decoding="async"
+            />
+          )}
+        </a>
+        <figcaption
+          class={`
+          absolute bottom-1 left-0 w-full flex flex-col gap-3 p-2 ${
+            l?.onMouseOver?.showSkuSelector || l?.onMouseOver?.showCta
+              ? "transition-opacity opacity-0 lg:group-hover:opacity-100"
+              : "lg:hidden"
+          }`}
+        >
+          {/* SKU Selector */}
+          {l?.onMouseOver?.showSkuSelector && (
+            <ul class="flex justify-center items-center gap-2 w-full">
+              {skuSelector}
+            </ul>
+          )}
+          {l?.onMouseOver?.showCta && cta}
+        </figcaption>
+      </figure>
+      {/* Prices & Name */}
+      <div class="flex-auto flex flex-col relative h-16  gap-1 lg:gap-2">
+        {/* SKU Selector */}
+        {
+          /* {(!l?.elementsPositions?.skuSelector ||
+          l?.elementsPositions?.skuSelector === "Top") && (
+          <>
+            {l?.hide?.skuSelector && variants.length ? "" : (
+              <ul
+                class={`flex items-center gap-2 w-full overflow-auto  ${
+                  align === "center" ? "justify-center" : "justify-start"
+                } ${l?.onMouseOver?.showSkuSelector ? "lg:hidden" : ""}`}
+              >
+                {skuSelector}
+              </ul>
+            )}
+          </>
+        )} */
+        }
+
+        {l?.hide?.productName && l?.hide?.productDescription
+          ? ""
+          : (
+            <div class="flex flex-col gap-0">
+              {l?.hide?.productName
+                ? ""
+                : (
+                  <h2 class="truncate text-xs text-base-content">
+                    {name}
+                  </h2>
+                )}
+            </div>
+          )}
+        {l?.hide?.allPrices ? "" : (
+          <div class="flex flex-col gap-2">
+            <div
+              class={`flex flex-col gap-0 ${
+                l?.basics?.oldPriceSize === "Normal"
+                  ? "lg:flex-row lg:gap-2"
+                  : ""
+              } ${align === "center" ? "justify-center" : "justify-start"}`}
+            >
+              {/* List Price */}
+              {
+                /* <div
+                class={`line-through text-base-300 text-xs ${
+                  l?.basics?.oldPriceSize === "Normal" ? "lg:text-xl" : ""
+                }`}
+              >
+                {formatPrice(listPrice, offers!.priceCurrency!)}
+              </div> */
+              }
+              <div class=" text-xs text-black font-bold ">
+                {formatPrice(price, offers!.priceCurrency!)}
+              </div>
+            </div>
+            {l?.hide?.installments && !installments ? "" : (
+              installments && (
+                <div class="text-base-300 text-xs">
+                  ou {installments}
+                </div>
+              )
+            )}
+          </div>
+        )}
+
+        {/* SKU Selector */}
+        {l?.elementsPositions?.skuSelector === "Bottom" && (
+          <>
+            {l?.hide?.skuSelector ? "" : (
+              <ul
+                class={`flex items-center gap-2 w-full ${
+                  align === "center" ? "justify-center" : "justify-start"
+                } ${l?.onMouseOver?.showSkuSelector ? "lg:hidden" : ""}`}
+              >
+                {skuSelector}
+              </ul>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ProductCardCategory;
